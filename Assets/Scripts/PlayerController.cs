a49217102e46dd79824a5a1778d49ad2473ea4c5
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-09-07 19:55:37 +0300

    Created player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    [SerializeField] private float _moveSpeed;
+   [SerializeField] private Animator _animator;
    private Vector3 _movement;
    public Vector3 Movement => _movement;
 
@@ -13,6 +14,8 @@ public class PlayerController : MonoBehaviour
    {
       _movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
       transform.position += _movement.normalized * (_moveSpeed * Time.deltaTime);
-      
+      _animator.SetFloat("Horizontal", _movement.x);
+      _animator.SetFloat("Vertical", _movement.y);
+      _animator.SetFloat("Speed", _movement.sqrMagnitude);
    }
 }
