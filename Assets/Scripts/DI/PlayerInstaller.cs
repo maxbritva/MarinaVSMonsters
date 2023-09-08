42ef4fb71e173bb37774cc062abb5cfedd93a12a
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-09-08 12:19:06 +0300

    Created enemy move

---
+++ Assets/Scripts/DI/PlayerInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Player;
+using UnityEngine;
+using Zenject;
+
+namespace DI
+{
+	public class PlayerInstaller : MonoInstaller
+	{
+		[SerializeField] private PlayerController _playerController;
+		public override void InstallBindings()
+		{
+			Container.Bind<PlayerController>().FromInstance(_playerController).AsSingle().NonLazy();
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Player;
+using UnityEngine;
+using Zenject;
+
+namespace Enemy
+{
+	public class EnemyMove : MonoBehaviour
+	{
+		[SerializeField] private float _moveSpeed;
+		private PlayerController _playerController;
+		private Vector3 _direction;
+
+		private void Update()
+		{
+			Move();
+			CheckDistanceToHide();
+		}
+
+		private void Move()
+		{
+			_direction = (_playerController.transform.position - transform.position).normalized;
+			transform.position += _direction * (_moveSpeed * Time.deltaTime);
+		}
+
+		private void CheckDistanceToHide()
+		{
+			float distance = Vector3.Distance(transform.position, _playerController.transform.position);
+			if(distance > 20f)
+				gameObject.SetActive(false);
+		}
+
+
+		[Inject] private void Construct(PlayerController playerController) => 
+			_playerController = playerController;
+	}
+}
