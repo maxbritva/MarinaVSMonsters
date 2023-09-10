0eeaf9b076eee83613f472b45559e498664dc9b2
maxbritva <71668634+maxbritva@users.noreply.github.com>
2023-09-10 21:41:35 +0300

    Created enemy animations

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Player;
 using UnityEngine;
 using Zenject;
@@ -7,8 +8,12 @@ namespace Enemy
 {
 	public class EnemyMove : MonoBehaviour
 	{
+		[SerializeField] private Animator _animator;
+		[SerializeField] private float _freezeTimer;
 		[SerializeField] private float _moveSpeed;
 		private PlayerController _playerController;
+		private float _initialMoveSpeed;
+		private WaitForSeconds _timer;
 		private Vector3 _direction;
 
 		private void Update()
@@ -17,10 +22,25 @@ namespace Enemy
 			CheckDistanceToHide();
 		}
 
+		private void Start()
+		{
+			_initialMoveSpeed = _moveSpeed;
+			_timer = new WaitForSeconds(_freezeTimer);
+		}
+
+		public void StopEnemy(bool value) => _moveSpeed = value ? 0f : _initialMoveSpeed;
+
+		public void Freeze()
+		{
+			if (gameObject.activeSelf)
+				StartCoroutine(StartFreeze());
+		}
 		private void Move()
 		{
 			_direction = (_playerController.transform.position - transform.position).normalized;
 			transform.position += _direction * (_moveSpeed * Time.deltaTime);
+			_animator.SetFloat("Horizontal", _direction.x);
+			_animator.SetFloat("Vertical", _direction.y);
 		}
 
 		private void CheckDistanceToHide()
@@ -30,6 +50,12 @@ namespace Enemy
 				gameObject.SetActive(false);
 		}
 
+		private IEnumerator StartFreeze()
+		{
+			_moveSpeed /= 2;
+			yield return _timer;
+			_moveSpeed = _initialMoveSpeed;
+		}
 
 		[Inject] private void Construct(PlayerController playerController) => 
 			_playerController = playerController;
