a97e59dc066b3129d05c1e49a2791d245bdf6cf6
Maxim Kiryakov <71668634+maxbritva@users.noreply.github.com>
2023-10-31 17:48:06 +0300

    Created enemy collision and health

---
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -0,0 +1,20 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyCollision : MonoBehaviour
+    {
+        [SerializeField] private float _damage;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.TryGetComponent(out PlayerHealth playerHealth))
+            {
+                if(playerHealth == null) return;
+                playerHealth.TakeDamage(_damage);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using GameCore.Health;
+using UnityEngine;
+
+
+namespace Enemy
+{
+    public class EnemyHealth : ObjectHealth
+    {
+        private WaitForSeconds _tick = new WaitForSeconds(0.5f);
+        public override void TakeDamage(float damage)
+        {
+            base.TakeDamage(damage);
+            if(_currentHealth <= 0 == false) return;
+            gameObject.SetActive(false);
+        }
+
+        public void Burn(float damage) => StartCoroutine(GetBurn(damage));
+
+        private IEnumerator GetBurn(float damage)
+        {
+            if(gameObject.activeSelf == false)
+                yield break;
+            float tickDamage = damage / 3f;
+            if (tickDamage < 1)
+                tickDamage = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                TakeDamage(tickDamage);
+                yield return _tick;
+            }
+        }
+    }
+}
