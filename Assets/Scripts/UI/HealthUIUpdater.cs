f1fa1b75940f65171cdab477f8ba4d902bb9457f
Maxim Kiryakov <71668634+maxbritva@users.noreply.github.com>
2023-11-03 18:03:21 +0300

    Created HP bar updater

---
+++ Assets/Scripts/UI/HealthUIUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Player;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI
+{
+    public class HealthUIUpdater : MonoBehaviour
+    {
+        [SerializeField] private Image _playerHealhImage;
+        private PlayerHealth _playerHealth;
+
+        private void OnEnable() => _playerHealth.OnHealthChanged += UpdateHealthBar;
+        private void OnDisable() => _playerHealth.OnHealthChanged -= UpdateHealthBar;
+
+        [Inject] private void Construct(PlayerHealth playerHealth) => _playerHealth = playerHealth;
+
+        private void UpdateHealthBar()
+        {
+            _playerHealhImage.fillAmount = _playerHealth.CurrentHealth / _playerHealth.MAXHealth;
+            _playerHealhImage.fillAmount = Mathf.Clamp01(_playerHealhImage.fillAmount);
+        }
+    }
+}
